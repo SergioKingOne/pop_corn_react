6c9b2e1409396e47d9eb54d4486fa89ea6d2e63f
Sergio <sergiorobayorr@gmail.com>
2023-09-02 11:57:48 -0500

    Solving API request issues

--- src/App-v2.js
+++ src/App-v2.js
@@ -37,7 +37,7 @@ export default function App() {
         setIsLoading(true);
         setError("");
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
           { signal: controller.signal }
         );
 
@@ -284,7 +284,7 @@ function MovieDetails({ selectedId, onCloseMovie, watched, onAddWatched }) {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
